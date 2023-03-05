aef89e2e557da2dbcf76f23713799e2543cc198f
s.pirohov <sergey.pirogov@gmail.com>
2023-03-05 17:10:39 +0200

    add report diff initial

--- src/main/java/io/perf/report/diff/DiffAnalyzer.java
+++ src/main/java/io/perf/report/diff/DiffAnalyzer.java
@@ -1,5 +1,6 @@
 package io.perf.report.diff;
 
+import de.vandermeer.asciitable.AT_Context;
 import de.vandermeer.asciitable.AsciiTable;
 import io.perf.report.model.RequestStats;
 import io.perf.report.model.SimulationStats;
@@ -10,18 +11,23 @@ import java.util.List;
 public class DiffAnalyzer {
 
     public static void computeDiff(SimulationStats baseSimulationStats, SimulationStats challengerSimulationStats) {
-        List<Diff> diffList = new ArrayList<>();
         List<RequestStats> baseSimulationStatsResults = baseSimulationStats.getResults();
         List<RequestStats> challengerSimulationStatsResults = challengerSimulationStats.getResults();
         System.out.println("=== Diff ===");
 
         AsciiTable at = new AsciiTable();
         at.addRule();
-        at.addRow("Request", "p95", "p99");
+        at.addRow("Request", "min", "p95", "p99", "max");
         at.addRule();
         for (int i = 0; i < baseSimulationStatsResults.size(); i++) {
             RequestStats baseRequestStats = baseSimulationStatsResults.get(i);
             RequestStats challengerRequestStats = challengerSimulationStatsResults.get(i);
+
+            long minLeft = baseRequestStats.getMin();
+            long minRight = challengerRequestStats.getMin();
+
+            long maxLeft = baseRequestStats.getMax();
+            long maxRight = challengerRequestStats.getMax();
 
             long p95Left = baseRequestStats.getP95();
             long p95Right = challengerRequestStats.getP95();
@@ -29,14 +35,19 @@ public class DiffAnalyzer {
             long p99Left = baseRequestStats.getP99();
             long p99Right = challengerRequestStats.getP99();
 
+            long minDiff = calculatePercentDiffPercent(minLeft, minRight);
             long p95Diff = calculatePercentDiffPercent(p95Left, p95Right);
             long p99Diff = calculatePercentDiffPercent(p99Left, p99Right);
+            long maxDiff = calculatePercentDiffPercent(maxLeft, maxRight);
 
             at.addRow(baseRequestStats.getRequestName(),
-                    p95Left + " -> " + p95Right + " (" + p95Diff + "%)",
-                    p99Left + " -> " + p99Right + " (" + p99Diff + "%)");
+                    minLeft + "->" + minRight + " (" + minDiff + "%)",
+                    p95Left + "->" + p95Right + " (" + p95Diff + "%)",
+                    p99Left + "->" + p99Right + " (" + p99Diff + "%)",
+                    maxLeft + "->" + maxRight + " (" + maxDiff + "%)"
+            );
+            at.addRule();
         }
-        at.addRule();
         String rend = at.render();
         System.out.println(rend);
         System.out.println("=== END ===");
