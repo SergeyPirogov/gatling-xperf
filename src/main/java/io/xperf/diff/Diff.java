a82fd5271d1a9b3cbd70f0030b0eaa00fab50fb1
SergeyPirogov <serhii.pirogov@gmail.com>
2023-03-06 17:42:23 +0200

    fix diff table

--- src/main/java/io/xperf/diff/Diff.java
+++ src/main/java/io/xperf/diff/Diff.java
@@ -5,17 +5,23 @@ public class Diff {
     private final String left;
     private final String right;
     private String requestName;
-    private String valueName;
 
-    public Diff(String requestName, String valueName, String left, String right) {
-        this.requestName = requestName;
-        this.valueName = valueName;
+    public Diff(String left, String right) {
         this.left = left;
         this.right = right;
     }
 
-    public static Diff of(String requestName, String valueName, String left, String right) {
-        return new Diff(requestName, valueName, left, right);
+    public static Diff of(String left, String right) {
+        return new Diff(left, right);
+    }
+
+    public static Diff of(long left, long right) {
+        return new Diff(String.valueOf(left), String.valueOf(right));
+    }
+
+    private static long calculatePercentDiffPercent(long left, long right) {
+        double res = ((double) (right - left) / right) * 100;
+        return Math.round(res);
     }
 
     public String getLeft() {
@@ -30,7 +36,19 @@ public class Diff {
         return requestName;
     }
 
-    public String getValueName() {
-        return valueName;
+    public String toString(boolean includePercent) {
+        if (includePercent) {
+            long percentDiffPercent = calculatePercentDiffPercent(Long.parseLong(left), Long.parseLong(right));
+            return left + " -> " + right + " (" + percentDiffPercent + "%)";
+        }
+        if (left.equals(right)) {
+            return left;
+        }
+        return left + " -> " + right;
+    }
+
+    @Override
+    public String toString() {
+        return toString(false);
     }
 }
--- src/main/java/io/xperf/diff/DiffAnalyzer.java
+++ src/main/java/io/xperf/diff/DiffAnalyzer.java
@@ -1,6 +1,10 @@
 package io.xperf.diff;
 
-import de.vandermeer.asciitable.AsciiTable;
+//import de.vandermeer.asciitable.AsciiTable;
+
+import com.github.freva.asciitable.AsciiTable;
+import com.github.freva.asciitable.Column;
+import com.github.freva.asciitable.HorizontalAlign;
 import io.xperf.model.RequestStats;
 import io.xperf.model.SimulationStats;
 
@@ -19,59 +23,63 @@ public class DiffAnalyzer {
                     baseSimulationStatsResults.size(), challengerSimulationStatsResults.size());
             System.out.println("Diff will be computed for lowest size");
         }
-        AsciiTable at = new AsciiTable();
-        at.addRule();
-        at.addRow("Request", "Max users", "OK", "KO", "p95", "p99", "stddev");
-        at.addRule();
+
+        String[][] data = new String[baseSimulationStatsResults.size()][];
+
         for (int i = 0; i < baseSimulationStatsResults.size(); i++) {
             RequestStats baseRequestStats = baseSimulationStatsResults.get(i);
 
             if (i >= challengerSimulationStatsResults.size()) {
                 break;
             }
-
             RequestStats challengerRequestStats = challengerSimulationStatsResults.get(i);
 
             long countUsersLeft = baseRequestStats.getMaxUsers();
             long countUsersRight = challengerRequestStats.getMaxUsers();
+            Diff usersDiff = Diff.of(countUsersLeft, countUsersRight);
 
             long leftSuccessCount = baseRequestStats.getSuccessCount();
             long rightSuccessCount = challengerRequestStats.getSuccessCount();
+            Diff successDiff = Diff.of(leftSuccessCount, rightSuccessCount);
 
             long leftErrorCount = baseRequestStats.getErrorCount();
             long rightErrorCount = challengerRequestStats.getErrorCount();
+            Diff errorsDiff = Diff.of(leftErrorCount, rightErrorCount);
 
             long p95Left = baseRequestStats.getP95();
             long p95Right = challengerRequestStats.getP95();
+            Diff p95Diff = Diff.of(p95Left, p95Right);
 
             long p99Left = baseRequestStats.getP99();
             long p99Right = challengerRequestStats.getP99();
+            Diff p99Diff = Diff.of(p99Left, p99Right);
 
-            long stddevLeft = baseRequestStats.getStddev();
-            long stddevRight = challengerRequestStats.getStddev();
+            long stdDevLeft = baseRequestStats.getStddev();
+            long stdDevRight = challengerRequestStats.getStddev();
+            Diff stdDiff = Diff.of(stdDevLeft, stdDevRight);
 
-            long p95Diff = calculatePercentDiffPercent(p95Left, p95Right);
-            long p99Diff = calculatePercentDiffPercent(p99Left, p99Right);
-            long stddevDiff = calculatePercentDiffPercent(stddevLeft, stddevRight);
+//            long p95Diff = calculatePercentDiffPercent(p95Left, p95Right);
+//            long p99Diff = calculatePercentDiffPercent(p99Left, p99Right);
+//            long stdDevDiff = calculatePercentDiffPercent(stdDevLeft, stdDevRight);
 
-            at.addRow(baseRequestStats.getRequestName(),
-                    countUsersLeft + "->" + countUsersRight,
-                    leftSuccessCount + "->" + rightSuccessCount,
-                    leftErrorCount + "->" + rightErrorCount,
-                    p95Left + "->" + p95Right + " (" + p95Diff + "%)",
-                    p99Left + "->" + p99Right + " (" + p99Diff + "%)",
-                    stddevLeft + "->" + stddevRight + " (" + stddevDiff + "%)"
-
-            );
-            at.addRule();
+            data[i] = new String[]{
+                    baseRequestStats.getRequestName(),
+                    usersDiff.toString(),
+                    successDiff.toString(),
+                    errorsDiff.toString(),
+                    p95Diff.toString(true),
+                    p99Diff.toString(true),
+                    stdDiff.toString(true)
+            };
         }
-        String rend = at.render();
-        System.out.println(rend);
-        System.out.println("=== END ===");
-    }
+        String[] headers = {"", "users", "OK", "KO", "p95", "p99", "stddev"};
+        String table = AsciiTable.builder()
+                .header(headers)
+                .data(data)
+                .border(AsciiTable.FANCY_ASCII)
+                .asString();
 
-    private static long calculatePercentDiffPercent(long left, long right) {
-        double res = ((double) (right - left) / right) * 100;
-        return Math.round(res);
+        System.out.println(table);
+        System.out.println("=== END ===");
     }
 }
